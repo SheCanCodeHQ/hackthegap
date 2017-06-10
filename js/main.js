3ff293636f793cae2082faa61537af63564c73f3
Matthew Bell <m.bell@cs.ucl.ac.uk>
2017-06-10 22:36:36 +0100

    Make nav menu items slide in incrementally

--- js/main.js
+++ js/main.js
@@ -14,17 +14,66 @@ NodeList.prototype.contains = function(element) {
 function onLoad() {
     const navButton = document.querySelector(".nav-button");
     let navButtonEls = navButton.querySelectorAll("*");
+    const navItems = document.querySelectorAll(".nav-right a");
+    let navItemsTimer;
+
+    const navItemDelay = 50;
+
+    // iterate through each nav item and expand it or shrink it.
+    // iterator: the iterator of navItem elements.
+    // isExpanding: true -> items get bigger. false -> items get smaller.
+    // done: callback for once this is finished.
+    const navItemsIterator = (iterator, isExpanding, done) => {
+        let item = iterator.next();
+        let navItem = item.value;
+        if (!item.done) {
+            if (isExpanding) {
+                navItem.setAttribute("style","width: "+navItem.getAttribute("data-width")+";");
+            }
+            else {
+                navItem.setAttribute("style", "width: 0;");
+            }
+        }
+        else {
+            clearInterval(navItemsTimer);
+            if (typeof(done) === "function") {
+                done();
+            }
+        }
+    }
+
     navButton.onclick = event => {
         let nav = document.querySelector("nav");
-        nav.classList.toggle("active");
-        navButton.classList.toggle("active");
+        let isActive = (nav.classList.contains("active"));
+        if (isActive) {
+            let eNavItems = [].slice.call(navItems,0).reverse()[Symbol.iterator]();
+            navItemsTimer = setInterval(navItemsIterator, navItemDelay, eNavItems, false, () => {
+                nav.classList.remove("active");
+                navButton.classList.remove("active");
+            });
+        }
+        else {
+            nav.classList.add("active");
+            navButton.classList.add("active");
+
+            // create an iterator of the navigation items, starting from the last one.
+            let eNavItems = [].slice.call(navItems,0).reverse()[Symbol.iterator]();
+            navItemsTimer = setInterval(navItemsIterator, navItemDelay, eNavItems, true);
+        }
     };
+
+    for (navItem of navItems) {
+        let width = navItem.scrollWidth;
+        navItem.setAttribute("data-width", width + "px");
+        navItem.setAttribute("style","width: 0;");
+    }
 
     window.onclick = event => {
         // only if event wasn't triggered by navButton
         if (!( (event.target === navButton) || navButtonEls.contains(event.target) )) {
             let nav = document.querySelector("nav");
             if (nav.classList.contains("active")) {
+                // close menu.
                 nav.classList.remove("active");
                 navButton.classList.remove("active");
             }
